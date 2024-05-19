2e63e425de88616eef6167effb9f322bbd0f2e11
fluffy <75144743+fluffeliger@users.noreply.github.com>
2024-05-19 15:56:25 +0200

    Added Header
    - Created Header Bar
    > With social links
    > With clickable header

--- app/ui/header/header.jsx
+++ app/ui/header/header.jsx
@@ -1,9 +1,37 @@
 import styles from './header.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+
+import discordSVG from '@/public/svgs/discord.svg'
+import githubSVG from '@/public/svgs/github.svg'
+import redditSVG from '@/public/svgs/reddit.svg'
 
 export default function Header() {
     return (
         <nav className={styles.header}>
-            <p>Das ist ein Header!</p>
+            <Link href='/'>
+                <h2 className={styles.title}>Hobbylos Lexikon</h2>
+            </Link>
+            <div className={styles.linkicons}>
+                <Link target='_blank' href='https://discord.com/invite/xfyWpPt3Uk'><Image
+                    src={discordSVG}
+                    width={30}
+                    height={30}
+                    alt='Discord'
+                /></Link>
+                <Link target='_blank' href='https://github.com/Lobby-Hoes/duden' ><Image
+                    src={githubSVG}
+                    width={30}
+                    height={30}
+                    alt='Github'
+                /></Link>
+                <Link target='_blank' href='https://www.reddit.com/r/Hobbylos/' ><Image
+                    src={redditSVG}
+                    width={30}
+                    height={30}
+                    alt='Reddit'
+                /></Link>
+            </div>
         </nav>
     );
 }
