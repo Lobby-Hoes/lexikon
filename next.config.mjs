7c1004402c3498350c2739a5d99d4749cf663632
Emil <95469786+eleander56-b@users.noreply.github.com>
2024-05-19 19:03:09 +0200

    maybe so
    Keine Ahnung xd kenn mich nicht wirklich mit nextjs aus

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,17 @@
 const nextConfig = {};
 
 export default nextConfig;
+module.exports = {
+  output: 'export',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/word/[word]': { page: '/word/[word]' },
+      '/wordNotFound': { page: '/wordNotFound' }
+    };
+  },
+  distDir: 'dist'
+};
