21d463cb6d400d15bfa0dc02075119edc6d287a7
fluffy <75144743+fluffeliger@users.noreply.github.com>
2024-05-19 20:25:43 +0200

    Build Folder Removal

--- app/word/[word]/dataFetcher.js
+++ app/word/[word]/dataFetcher.js
@@ -22,25 +22,3 @@ export default async function fetchData(word) {
     if (finalData.length == 0) return null;
     return finalData[0];
 }
-
-export async function fetchWholeData() {
-    let data;
-    try {
-        data = await fetch('https://data.hobbylos.online/graphql', {
-            body: JSON.stringify({ query: '{ word { word type explanation examples closeWords } }' }),
-            headers: { 'Content-Type': 'application/json' },
-            method: 'POST'
-        });
-        if (!data) return null;
-    }
-    catch { return null; }
-    let json;
-    try {
-        json = await data.json();
-        if (!json) return null;
-    }
-    catch { return null }
-    const finalData = json.data.word;
-    if (finalData.length == 0) return null;
-    return finalData;
-}
